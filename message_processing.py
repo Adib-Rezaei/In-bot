b0882ea5a2888eec4cefd69c5537258b6986cc51
Adib-Rezaei <adibrezaeish@gmail.com>
2021-04-26 16:51:26 +0430

    refactor

--- message_processing.py
+++ message_processing.py
@@ -5,7 +5,5 @@ from setting import Setting
 def tof_counter(update: Update, _: CallbackContext) -> None:
     """Echo the user message."""
     # update.message.reply_text(update.message.text)
-    global cnt
     num = update.message.text.count('تف تو قبر امیر')
     Setting.inc_cnt(num)
-    # print(cnt)
